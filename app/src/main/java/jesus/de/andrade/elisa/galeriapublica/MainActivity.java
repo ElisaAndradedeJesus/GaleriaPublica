b9a974fbe1aa9efeb2318cd86547977ee5d1269b
sala206_CAMPUZ <elisaaj.es@gmail.com>
2023-08-29 11:06:26 -0300

    atualizando

--- app/src/main/java/jesus/de/andrade/elisa/galeriapublica/MainActivity.java
+++ app/src/main/java/jesus/de/andrade/elisa/galeriapublica/MainActivity.java
@@ -1,14 +1,25 @@
 package jesus.de.andrade.elisa.galeriapublica;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+
+        bottomNavigationView = findViewById(R.id.btNav);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener())
     }
 }
