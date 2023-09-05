6b8af9a4702aaf5c27da778b518af613c0ae8e6c
20201TIIMI0225 <elisaaj.es@gmail.com>
2023-09-05 09:14:16 -0300

    Resolvido problema com paging 3

--- app/src/main/java/jesus/de/andrade/elisa/galeriapublica/MainViewModel.java
+++ app/src/main/java/jesus/de/andrade/elisa/galeriapublica/MainViewModel.java
@@ -4,13 +4,22 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
+import androidx.paging.PagingData;
 
 public class MainViewModel extends AndroidViewModel {
 
     int navigationOpSelected = R.id.gridViewOp;
+    LiveData<PagingData<ImageData>> pageLv;
 
     public MainViewModel(@NonNull Application application){
         super(application);
+        GalleryRepository galleryRepository = new GalleryRepository(application);
+        GalleryPagingSource galleryPagingSource = new GalleryPagingSource(galleryRepository);
+        Pager<Integer, ImageData> pager = new Pager(new PagingConfig(10), () -> galleryPagingSource);
+        //linha 11
     }
 
     public int getNavigationOpSelected() {
